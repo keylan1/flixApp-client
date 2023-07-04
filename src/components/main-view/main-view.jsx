62c2d3f2166aae0896cddea096dab4af8cf8e9ac
Sarah.Keller <sarah29.84@gmail.com>
2023-07-04 02:20:10 +0100

    Updated movie info

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,22 +8,34 @@ export const MainView = () => {
       id: 1,
       title: 'The Proposal',
       image:
-        'https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg',
-      director: 'Marijn Haverbeke',
+        'https://www.imdb.com/title/tt1041829/mediaviewer/rm2227985664/?ref_=tt_ov_i',
+      description:
+        'The romance genre focuses on interpersonal relationships, love, and emotional connections between characters, often involving themes of passion, affection, and romantic experiences.',
+      genre: 'Romance',
+      director: 'Anne Fletcher',
+      year: 2009,
     },
     {
       id: 2,
-      title: 'Inception',
+      title: 'The Lord of the Rings: The Fellowship of the Ring',
       image:
-        'https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-      director: 'Federico Kereki',
+        'https://www.imdb.com/title/tt0120737/mediaviewer/rm3592958976/?ref_=tt_ov_i',
+      description:
+        'A hobbit named Frodo embarks on a perilous journey to destroy a powerful ring and save Middle-earth from the dark forces of Sauron.',
+      genre: 'Fantasy',
+      director: 'Peter Jackson',
+      year: 2001,
     },
     {
       id: 3,
       title: 'The Dark Knight',
       image:
         'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg',
+      description:
+        'Batman faces his ultimate challenge as he battles the Joker, a psychopathic mastermind bent on wreaking havoc on Gotham City.',
+      genre: 'Action',
       director: 'Douglas Crockford',
+      year: 2008,
     },
   ]);
 

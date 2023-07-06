6bc3df671d06a591e30284831ad732bb5b883967
Sarah.Keller <sarah29.84@gmail.com>
2023-07-06 01:06:21 +0100

    Updated

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -5,28 +5,38 @@ export const MovieView = ({ movie, onBackClick }) => {
         <img
           crossOrigin="anonymous"
           style={{ width: '400px', height: '500px' }}
-          src={movie.image}
+          src={movie.ImagePath}
         />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{movie.description}</span>
+        <span>{movie.Description}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.genre}</span>
+        <span>{movie.Genre.Name}</span>
+        <span>{movie.Genre.Description}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{movie.Director.Name}</span>
+        <span>{movie.Director.Bio}</span>
       </div>
       <div>
         <span>Year: </span>
-        <span>{movie.year}</span>
+        <span>{movie.Year}</span>
+      </div>
+      <div>
+        <span>Actors: </span>
+        <span>{movie.Actors}</span>
+      </div>
+      <div>
+        <span>Featured: </span>
+        <span>{movie.Featured}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
